50c87d4fedb310257a9db5fedd551655893862fc
Eliot Jang <eliotjang2@gmail.com>
2020-09-23 19:50:19 +0900

    sample(c) 데이터 전달
    전처리를 하는 코드를 수정하였다.
    기존에는 1개의 json이었지만,
    지금은 6~7개의 json으로 늘어남에따라서 파일을 생성하고 전역배열에 집어넣는 함수를 구현하였다.
    
    또한 전처리0921, 전처리0923 형식의 csv파일을 만들어 구분하였다.

--- JSON_TO_CSV/preprocessing-data.py
+++ JSON_TO_CSV/preprocessing-data.py
@@ -4,7 +4,6 @@ import json
 import csv
 from io import open
 
-file_path = "walkingresearch-b7378-export.json"
 init_counter = 1 # 초기화할 생각이 있으면 0으로 설정.
 tmp_x = 0.0
 tmp_y = 0.0
@@ -12,29 +11,28 @@ tmp_z = 0.0
 contain_x = []
 contain_y = []
 contain_z = []
-name = "sample(A)"
+name = "sample(C)"
 
-with open(file_path, "r", encoding='UTF8') as json_file:
-    json_data = json.load(json_file)
+def AppendArr(file_path_name):
+    with open(file_path_name, "r", encoding='UTF8') as json_file:
+        json_data = json.load(json_file)
 
-    for x in json_data['RecordAccelerometer'][name + '2']['x'].values():
-        #tmp_x = float(x)
-        #if tmp_x <= 0.001 and tmp_x >= -0.001:
-        #    x = "0"
-        contain_x.append(float(x))
-    for y in json_data['RecordAccelerometer'][name + '2']['y'].values():
-        #tmp_y = float(y)
-        #if tmp_y <= 0.001 and tmp_y >= -0.001:
-        #    y = "0"
-        contain_y.append(float(y))
-    for z in json_data['RecordAccelerometer'][name + '2']['z'].values():
-        #tmp_z = float(z)
-        #if tmp_z <= 0.001 and tmp_z >= -0.001:
-        #    z = "0"
-        contain_z.append(float(z))
-json_file.close()
+        for x in json_data['RecordAccelerometer'][name + '2']['x'].values():
+            contain_x.append(float(x))
+        for y in json_data['RecordAccelerometer'][name + '2']['y'].values():
+            contain_y.append(float(y))
+        for z in json_data['RecordAccelerometer'][name + '2']['z'].values():
+            contain_z.append(float(z))
+    json_file.close()
 
-with open('전처리.csv', 'a', newline='') as f:
+AppendArr("walkingresearch-b7378-export (1).json")
+AppendArr("walkingresearch-b7378-export (2).json")
+AppendArr("walkingresearch-b7378-export (3).json")
+AppendArr("walkingresearch-b7378-export (4).json")
+AppendArr("walkingresearch-b7378-export (5).json")
+AppendArr("walkingresearch-b7378-export (6).json")
+
+with open('전처리0924.csv', 'a', newline='') as f:
     fieldnames = ['user', 'x', 'y', 'z', 'label(elder)']
     thewriter = csv.DictWriter(f, fieldnames=fieldnames)
 
